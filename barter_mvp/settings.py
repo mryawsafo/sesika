14f9d4fd2e735b89e50cdfb1dab3217096441893
mryawsafo <58732142+mryawsafo@users.noreply.github.com>
2026-05-11 03:34:05 +0000

    Add PostgreSQL support via dj-database-url

--- barter_mvp/settings.py
+++ barter_mvp/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,12 +58,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'barter_mvp.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = []
 
